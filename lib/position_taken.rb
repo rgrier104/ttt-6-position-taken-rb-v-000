9c480cceb08cc29656d3287000f3aee61214d98e
Rachel Grier <rgrier104@gmail.com>
2018-08-12 23:06:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,7 @@ def position_taken?(board, index)
   index_number = index - 1
   if board[index_number] = "X" || board[index_number] = "O"
     true
+  end
   if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
     false
   end
