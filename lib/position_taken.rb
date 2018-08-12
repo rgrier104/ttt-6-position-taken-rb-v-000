d05f72ed992e5016e6d77e03e8d84b71b6aed5c3
Rachel Grier <rgrier104@gmail.com>
2018-08-12 22:32:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   index_number = index - 1
-  if board[index_number] == " "
+  if board[index_number] != " "
+    
   end
 end
